21f76384f3f50c8db13a8b31e4dced699271b7ac
Sri Abhishek Mamidi <66781982+sam5747@users.noreply.github.com>
2024-05-15 11:40:32 +0000

    Assignment changes

---
+++ src/components/Body.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import lake from "../image/lake.JPG";
+
+const imageData = [
+  { id: 1, img: lake },
+  { id: 2, img: lake },
+  { id: 3, img: lake },
+  { id: 4, img: lake },
+];
+
+export default function Body() {
+  return (
+    <div>
+      {imageData.map((image) => (
+        <div key={image.id} className="column">
+          <img src={image.img} alt=""></img>
+        </div>
+      ))}
+    </div>
+  );
+}
